61dd1c0cfc9c8d5e84bd2beebca9c2c38a46ad03
kurt <kurtjacob.urquico@gmail.com>
2022-12-06 14:44:47 +0800

    added Loading.jsx

--- client/src/layouts/Loading.jsx
+++ client/src/layouts/Loading.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Loader, Center, useMantineTheme } from "@mantine/core";
 
-function Loading() {
-  return <div>Loading</div>;
+export default function Loading() {
+  const theme = useMantineTheme();
+
+  return (
+    <Center
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+      }}
+    >
+      <Loader />
+    </Center>
+  );
 }
-
-export default Loading;
