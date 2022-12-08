a751573ddc431839e8bb0faa6c7b818871bbe0f7
kurt <kurtjacob.urquico@gmail.com>
2022-12-09 00:12:36 +0800

    add anonymous user UI while posting

--- client/src/layouts/WritePostCard.jsx
+++ client/src/layouts/WritePostCard.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { useMantineTheme, Text, Switch } from "@mantine/core";
+import React, { useState } from "react";
+import { useMantineTheme, Text, Switch, Select } from "@mantine/core";
 import User from "./User";
 
 function WritePostCard() {
+  const [isAnonymous, setIsAnonymous] = useState(false);
   const theme = useMantineTheme();
 
   return (
@@ -21,20 +22,24 @@ function WritePostCard() {
         flexDirection: "column",
       }}
     >
-      <PostAnonymously />
+      <PostAnonymously
+        setIsAnonymous={setIsAnonymous}
+        isAnonymous={isAnonymous}
+      />
       <User
         publisher={localStorage.getItem("name")}
-        isAnonymous={false}
+        isAnonymous={isAnonymous}
         email={localStorage.getItem("email")}
         isAdmin={""}
         isCurrentUserAdmin={false}
         hideTrashAndBadge={true}
       />
+      <ChooseCategory />
     </div>
   );
 }
 
-function PostAnonymously() {
+function PostAnonymously({ setIsAnonymous, isAnonymous }) {
   const theme = useMantineTheme();
 
   return (
@@ -42,11 +47,10 @@ function PostAnonymously() {
       style={{
         width: "100%",
         height: "auto",
-        backgroundColor: theme.colorScheme === "dark" ? "#2C2E33" : "#f4f4f4",
+        backgroundColor: theme.colorScheme === "dark" ? "#25262B" : "#f4f4f4",
         borderRadius: "4px",
         border: "1px solid",
-        borderColor:
-          theme.colorScheme === "dark" ? theme.colors.gray[7] : "#CED4DA",
+        borderColor: theme.colorScheme === "dark" ? "#373A40" : "#CED4DA",
         display: "flex",
         padding: "0.75rem",
         marginBottom: "1rem",
@@ -69,12 +73,29 @@ function PostAnonymously() {
         style={{
           marginTop: "-1rem",
         }}
+        checked={isAnonymous}
+        onChange={(event) => setIsAnonymous(event.currentTarget.checked)}
         labelPosition="left"
         onLabel="ANONYMOUS"
         offLabel={`YOU`}
         size="lg"
       />
     </div>
+  );
+}
+
+function ChooseCategory() {
+  return (
+    <>
+      <Select
+        style={{ marginLeft: "2.90rem", width: "150px" }}
+        placeholder="Category"
+        data={[
+          { value: "personal", label: "personal" },
+          { value: "academic", label: "academic" },
+        ]}
+      />
+    </>
   );
 }
 
