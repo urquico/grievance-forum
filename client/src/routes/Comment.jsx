21d70b9665689209dd6928bed4a4bddb0fb8b385
kurt <kurtjacob.urquico@gmail.com>
2022-12-19 19:12:34 +0800

    added rte on comment

--- client/src/routes/Comment.jsx
+++ client/src/routes/Comment.jsx
@@ -1,11 +1,14 @@
 import React, { useLayoutEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { RichTextEditor } from "@mantine/rte";
+import { Button } from "@mantine/core";
 
 import Frame from "../layouts/Frame/Frame";
 import IntroductionCard from "../layouts/IntroductionCard";
 import PostCard from "../layouts/PostCard";
 import LoadingPost from "../layouts/Loading/LoadingPost";
 import { getSinglePost } from "../firebase-config";
+import { useMantineTheme } from "@mantine/core";
 
 function Comment() {
   let { id } = useParams();
@@ -13,12 +16,16 @@ function Comment() {
 }
 
 function CommentLayout() {
+  const initialValue = "<p><b>Share</b> your <i>thoughts</i> ...</p>";
+
   let { id } = useParams();
   const [post, setPost] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [timeCurrent, setTimeCurrent] = useState(0);
   const [timePosted, setTimePosted] = useState(0);
   const [hour, setHour] = useState(0);
+  const [text, setText] = useState(initialValue);
+  const theme = useMantineTheme();
 
   useLayoutEffect(() => {
     getSinglePost(id).then((result) => {
@@ -43,7 +50,21 @@ function CommentLayout() {
       {isLoading ? (
         <LoadingPost />
       ) : (
-        <>
+        <div
+          style={{
+            display: "flex",
+            height: "auto",
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+            borderRadius: "13px",
+            padding: "2.375rem",
+            marginTop: "1rem",
+            boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+            flexDirection: "column",
+          }}
+        >
           <PostCard
             isAnonymous={post.isAnonymous}
             email={post.userId}
@@ -56,9 +77,46 @@ function CommentLayout() {
             voteNumber={post.upVote - post.downVote}
             previewOnly={false}
           />
-        </>
+          <div
+            style={{
+              display: "flex",
+              height: "auto",
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+              marginTop: "1rem",
+
+              borderRadius: "13px",
+              padding: "2.375rem",
+
+              boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+              flexDirection: "column",
+            }}
+          >
+            <RichTextBox text={text} setText={setText} />
+            <Button style={{ marginTop: "0.500rem" }}>Submit Comment</Button>
+          </div>
+        </div>
       )}
     </div>
+  );
+}
+
+function RichTextBox({ text, setText }) {
+  return (
+    <RichTextEditor
+      style={{ marginTop: "1rem", zIndex: "1" }}
+      value={text}
+      onChange={setText}
+      sticky={true}
+      stickyOffset={60}
+      controls={[
+        ["bold", "italic", "underline", "link", "strike", "clean"],
+        ["unorderedList", "orderedList", "h1", "h2", "h3", "h4"],
+        ["sup", "sub", "blockquote", "code"],
+      ]}
+    />
   );
 }
 
