b1fdb9a8066f07e661b25213205e9ea0692dcd7f
kurtjacob22 <kurtjacob.urquico@gmail.comb>
2023-06-02 21:57:30 +0800

    fixed buts on tagLinks UI

--- client/src/layouts/Frame/TagLinks.jsx
+++ client/src/layouts/Frame/TagLinks.jsx
@@ -10,23 +10,25 @@ function TagLinks({ tag, tagCount }) {
     navigate(`/tags/${tag}`);
   };
 
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "auto",
-        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
-        padding: "0.75rem",
-        borderRadius: "13px",
-        boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-      }}
-    >
-      <Text fz="l" fw={700} tt="uppercase" style={{ cursor: "pointer" }} onClick={path}>
-        {tag}
-      </Text>
-      <Text fz="xs">{tagCount.toLocaleString("en-US")} Posts</Text>
-    </div>
-  );
+  if (tagCount !== 0) {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "auto",
+          backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+          padding: "0.75rem",
+          borderRadius: "13px",
+          boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+        }}
+      >
+        <Text fz="l" fw={700} tt="uppercase" style={{ cursor: "pointer" }} onClick={path}>
+          {tag}
+        </Text>
+        <Text fz="xs">{tagCount.toLocaleString("en-US")} Posts</Text>
+      </div>
+    );
+  }
 }
 
 export default TagLinks;
