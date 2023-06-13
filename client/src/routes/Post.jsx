7e206341997b61dae1b736411eda1537f2b6c5ac
kurtjacob22 <kurtjacob.urquico@gmail.comb>
2023-06-13 08:12:25 +0800

    fixed title for post

--- client/src/routes/Post.jsx
+++ client/src/routes/Post.jsx
@@ -26,7 +26,7 @@ function PostLayout() {
     <div>
       <IntroductionCard
         name={localStorage.getItem("name")}
-        message={`<br/>This is a safe place to share and express your thoughts, concerns, and grievances personally or academically.
+        message={`<br/>This is a safe place to share and express your thoughts, concerns, and grievances.
   <br/><br/><i>Please <b>avoid the use of offensive language</b> that would make others uncomfortable. </i>
   <br/><br/>Let's make this platform fun and safe for everyone!
         `}
